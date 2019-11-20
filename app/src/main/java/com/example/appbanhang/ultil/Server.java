2ca9773182f9fe159ea83d31bff47877640485b4
MaiLinh1504 <57564857+MaiLinh1504@users.noreply.github.com>
2019-11-20 13:53:40 -0800

    Initial commit

---
+++ app/src/main/java/com/example/appbanhang/ultil/Server.java
@@ -0,0 +1,8 @@
+package com.example.appbanhang.ultil;
+
+public class Server {
+    public static String localhost="192.168.43.254";
+    public static String DuongdanLoaisp="http://"+localhost+"/server/getloaisanpham.php";
+    public static String Duongdansanphammoinhat="http://"+localhost+"/server/getsanpham.php";
+
+}
